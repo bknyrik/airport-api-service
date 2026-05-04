b55d0c5d23546bc0dcc1ea0e4a686b5b83075f77
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-04 16:27:23 +0300

    Add model Ticket

--- airport/models.py
+++ airport/models.py
@@ -122,3 +122,18 @@ class Order(models.Model):
 
     def __str__(self) -> str:
         return str(self.created_at)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(
+        Flight,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
