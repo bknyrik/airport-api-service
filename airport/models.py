1970c92920d831bea4e5036d2d98fe4bce8e44e1
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-04 16:34:54 +0300

    Fix conflict with option related_name of source and destination fields in Route

--- airport/models.py
+++ airport/models.py
@@ -52,12 +52,12 @@ class Route(models.Model):
     source = models.ForeignKey(
         Airport,
         on_delete=models.CASCADE,
-        related_name="routes"
+        related_name="source_routes"
     )
     destination = models.ForeignKey(
         Airport,
         on_delete=models.CASCADE,
-        related_name="routes"
+        related_name="destination_routes"
     )
     distance = models.IntegerField()
 
