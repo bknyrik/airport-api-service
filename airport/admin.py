bfc2b69d3568c1e753cb38e333c072a0f125b81a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-06 15:48:02 +0300

    Register models Facility and AirplaneType to admin panel

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import Facility, AirplaneType
+
+
+admin.site.register(Facility)
+admin.site.register(AirplaneType)
