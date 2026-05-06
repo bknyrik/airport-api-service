56be078484b5124147b36e3a40902ed1ec7c993b
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-06 11:48:27 +0300

    Update full_name and __str__ of Crew

--- airport/models.py
+++ airport/models.py
@@ -119,10 +119,10 @@ class Crew(models.Model):
 
     @property
     def full_name(self) -> str:
-        return "%s %s" % (self.first_name, self.last_name)
+        return f"{self.first_name} {self.last_name}"
 
     def __str__(self) -> str:
-        return f"%s - %s" % (self.full_name, self.get_role_display())
+        return f"{self.full_name} - {self.get_role_display()}"
 
 
 class Flight(models.Model):
