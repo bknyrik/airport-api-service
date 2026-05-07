231437ccb537665e0073386d1df4f6ec055dc6e4
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-07 06:48:49 +0300

    Register models Airplane and Airport to admin panel

--- airport/admin.py
+++ airport/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
-from airport.models import Facility, AirplaneType
+from airport.models import (
+    Facility,
+    AirplaneType,
+    Airplane,
+    Airport
+)
 
 
 admin.site.register(Facility)
 admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Airport)
