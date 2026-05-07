a7f418b445ea6b5aee0edf1c62466a88b529cf95
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-07 06:50:33 +0300

    Register model Route to admin panel

--- airport/admin.py
+++ airport/admin.py
@@ -4,7 +4,8 @@ from airport.models import (
     Facility,
     AirplaneType,
     Airplane,
-    Airport
+    Airport,
+    Route,
 )
 
 
@@ -12,3 +13,4 @@ admin.site.register(Facility)
 admin.site.register(AirplaneType)
 admin.site.register(Airplane)
 admin.site.register(Airport)
+admin.site.register(Route)
