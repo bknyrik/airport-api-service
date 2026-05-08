07b15fe16792550b78b2cab05cb8ce09c1a94d74
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-08 14:20:37 +0300

    Register set of routers with AirplaneViewSet

--- airport/urls.py
+++ airport/urls.py
@@ -2,13 +2,15 @@ from rest_framework.routers import DefaultRouter
 
 from airport.views import (
     FacilityViewSet,
-    AirplaneTypeViewSet
+    AirplaneTypeViewSet,
+    AirplaneViewSet
 )
 
 
 router = DefaultRouter()
 router.register("facilities", FacilityViewSet)
 router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
 
 
 urlpatterns = router.urls
