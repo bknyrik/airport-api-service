6b404450ea374caca9256ea0b85dc0a6dba743d9
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-08 13:51:42 +0300

    Add serializer AirplaneTypeSerializer

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
-from airport.models import Facility
+from airport.models import (
+    Facility,
+    AirplaneType
+)
 
 
 class FacilitySerializer(serializers.ModelSerializer[Facility]):
     class Meta:
         model = Facility
         fields = "__all__"
+
+
+class AirplaneTypeSerializer(serializers.ModelSerializer[AirplaneType]):
+    class Meta:
+        model = AirplaneType
+        field = "__all__"
