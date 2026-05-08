44b0fbc799fc636ee9775a83be7b0132245cccd0
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-08 13:53:32 +0300

    Add viewset AirplaneTypeViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,9 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
 
-from airport.models import Facility
-from airport.serializers import FacilitySerializer
+from airport.models import (
+    Facility,
+    AirplaneType
+)
+from airport.serializers import (
+    FacilitySerializer,
+    AirplaneTypeSerializer
+)
 
 
 class FacilityViewSet(ModelViewSet):
     queryset = Facility.objects.all()
     serializer_class = FacilitySerializer
+
+
+class AirplaneTypeViewSet(ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
