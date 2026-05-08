389d890fbcad0fb08bcc539ade8b22acb4080a67
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-08 13:26:28 +0300

    Add viewset FacilityViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from airport.models import Facility
+from airport.serializers import FacilitySerializer
+
+
+class FacilityViewSet(ModelViewSet):
+    queryset = Facility.objects.all()
+    serializer_class = FacilitySerializer
