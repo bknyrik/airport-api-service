0369fc6b343f58c70aaee87ff4b7961b93c1bd81
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-08 13:49:29 +0300

    Add namespace with airport urls to urlpatterns of project urls.py

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls"))
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("api/airport/", include("airport.urls", namespace="airport")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
