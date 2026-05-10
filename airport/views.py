53b8161cb50f060bb2d52190da02367ab629ab1c
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-10 21:53:44 +0300

    Add attribute lookup_field to CrewViewSet

--- airport/views.py
+++ airport/views.py
@@ -115,6 +115,7 @@ class AirportViewSet(ModelViewSet):
 
 
 class CrewViewSet(ModelViewSet):
+    lookup_field = "slug"
     queryset = Crew.objects.all()
 
     def get_serializer_class(self) -> type[CrewSerializer]:
