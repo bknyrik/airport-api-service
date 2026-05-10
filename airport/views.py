34d73d043192591e1eecffc83b95f1bcdecfef3c
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-10 15:53:32 +0300

    Add action upload_image and update get_serializer_class of AirplaneViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,4 +1,8 @@
+from rest_framework.request import Request
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
 
 from airport.models import (
     Facility,
@@ -10,7 +14,8 @@ from airport.serializers import (
     AirplaneTypeSerializer,
     AirplaneSerializer,
     AirplaneListSerializer,
-    AirplaneRetrieveSerializer
+    AirplaneRetrieveSerializer,
+    AirplaneImageSerializer
 )
 
 
@@ -34,4 +39,19 @@ class AirplaneViewSet(ModelViewSet):
         if self.action == "retrieve":
             return AirplaneRetrieveSerializer
 
+        if self.action == "upload_image":
+            return AirplaneImageSerializer
+
         return AirplaneSerializer
+
+    @action(
+        methods=("POST",),
+        detail=True,
+        url_path="upload_image",
+    )
+    def upload_image(self, request: Request, pk: int = None) -> Response:
+        airplane = self.get_object()
+        serializer = self.get_serializer(airplane, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
