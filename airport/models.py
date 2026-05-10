812e3e18a7600a9b7ef22179353c3132f21930f0
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-10 19:41:07 +0300

    Add fields description and slug to Facility

--- airport/models.py
+++ airport/models.py
@@ -24,6 +24,8 @@ def create_custom_image_path(
 
 class Facility(models.Model):
     name = models.CharField(max_length=32, unique=True)
+    description = models.TextField(null=True, blank=True)
+    slug = models.SlugField()
 
     class Meta:
         verbose_name_plural = "facilities"
