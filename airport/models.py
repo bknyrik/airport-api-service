6d3ab0d9004537e8a659cc25298a104d3f3b6ccc
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-26 23:25:52 +0300

    Add static method validate_row_and_seat to Ticket

--- airport/models.py
+++ airport/models.py
@@ -237,6 +237,24 @@ class Ticket(models.Model):
             ),
         )
 
+    @staticmethod
+    def validate_row_and_seat(
+        row: int,
+        seat: int,
+        max_rows: int,
+        max_seats: int,
+        exception_type: type[Exception]
+    ) -> None:
+        if row not in range(1, max_rows + 1):
+            raise exception_type(
+                {"row": f"Row must be in range from 1 to {max_rows}"}
+            )
+
+        if seat not in range(1, max_seats + 1):
+            raise exception_type(
+                {"seat": f"Row must be in range from 1 to {max_seats}"}
+            )
+
     def clean(self) -> None:
         rows = self.flight.airplane.rows
         seats_in_row = self.flight.airplane.seats_in_row
