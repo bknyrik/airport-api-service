079cb45db964f00f2cb9ad928d6d8e2b97088d9a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-28 12:18:31 +0300

    Add filtering logic by iata_code in get_queryset of AirportViewSet

--- airport/views.py
+++ airport/views.py
@@ -83,12 +83,16 @@ class AirportViewSet(ModelViewSet):
         if self.action == "list":
             country = self.request.query_params.get("country")
             city = self.request.query_params.get("city")
+            iata_code = self.request.query_params.get("iata_code")
 
             if country:
                 queryset = queryset.filter(country__iexact=country)
 
             if city:
                 queryset = queryset.filter(city__iexact=city)
+
+            if iata_code:
+                queryset = queryset.filter(iata_code__icontains=iata_code)
 
         return queryset
 
