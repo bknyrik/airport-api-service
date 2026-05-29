76bddb232a22509b9eb91a1bc6844d788188cf06
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-29 21:57:00 +0300

    Add pagination class FlightSetPagination

--- airport/pagination.py
+++ airport/pagination.py
@@ -23,3 +23,9 @@ class RouteSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 20
+
+
+class FlightSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 10
