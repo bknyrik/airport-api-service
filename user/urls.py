d2362f18011d30bd6ddae5642f9d6968299ffba9
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-30 16:57:58 +0300

    Add path with ManageUserAPIView to urlpatterns of user urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserAPIView
+from user.views import CreateUserAPIView, ManageUserAPIView
 
 
 urlpatterns = [
     path("register/", CreateUserAPIView.as_view(), name="create"),
+    path("me/", ManageUserAPIView.as_view(), name="manage")
 ]
 
 app_name = "user"
