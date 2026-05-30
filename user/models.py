c09f2676993022e6587fc60e53f0c4f60a04c05a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-30 08:56:23 +0300

    Override field email to User

--- user/models.py
+++ user/models.py
@@ -1,5 +1,8 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
     username = None
+    email = models.EmailField(_("Email address"), unique=True)
