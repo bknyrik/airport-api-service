a76694b76e1af501a14f69dbf25588884fd1c317
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-30 09:25:08 +0300

    Add admin class UserAdmin to register model User

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext as _
 
 
-admin.site.register(get_user_model(), UserAdmin)
+@admin.register(get_user_model())
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (_("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions"
+                )
+            }
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")})
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide", ),
+                "fields": ("email", "password1", "password2")
+            }
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
