fa7e1e78ed2cbc3b4471f7986bf162e99d995fcd
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-30 16:57:22 +0300

    Add view ManageUserAPIView

--- user/views.py
+++ user/views.py
@@ -1,7 +1,18 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
 
 from user.serializers import UserSerializer
 
 
+User = get_user_model()
+
+
 class CreateUserAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class ManageUserAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self) -> User:
+        return self.request.user
