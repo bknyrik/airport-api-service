5e310b534e8fd4891cf389d3a4a531db22b28289
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-30 09:00:32 +0300

    Add manager UserManager

--- user/models.py
+++ user/models.py
@@ -1,6 +1,15 @@
+from __future__ import annotations
+
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    AbstractUser,
+    UserManager as DjangoUserManager
+)
 from django.utils.translation import gettext as _
+
+
+class UserManager(DjangoUserManager):
+    use_in_migrations = True
 
 
 class User(AbstractUser):
