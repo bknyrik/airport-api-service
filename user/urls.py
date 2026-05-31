5d96fa1a64986e2dd0a5b535cea39947226d5e50
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-31 22:08:31 +0300

    Add paths with TokenRefreshView and TokenObtainPairView to urlpatterns of user urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenObtainPairView,
+)
 
 from user.views import (
     RegisterUserAPIView,
@@ -13,6 +17,12 @@ router.register("", UserAdminViewSet)
 
 
 urlpatterns = [
+    path("token/obtain/", TokenObtainPairView.as_view(), name="token_obtain"),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
     path("register/", RegisterUserAPIView.as_view(), name="create"),
     path("me/", ManageUserAPIView.as_view(), name="manage"),
     path("", include(router.urls)),
