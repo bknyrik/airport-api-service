cea7af984b85114a27c59dc0135a3bf3e070fbde
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-31 10:16:37 +0300

    Add viewset UserViewSet

--- user/views.py
+++ user/views.py
@@ -1,10 +1,21 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, UserAdminSerializer
 
 
 User = get_user_model()
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.prefetch_related("user_permissions", "groups")
+
+    def get_serializer_class(self) -> type[UserSerializer]:
+        if self.request.user.is_superuser:
+            return UserAdminSerializer
+
+        return UserSerializer
 
 
 class CreateUserAPIView(generics.CreateAPIView):
