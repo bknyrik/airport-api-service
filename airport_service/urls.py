80f3711b50a9502b506ab4fa1f3ccd2de7d5112e
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-05-31 18:39:14 +0300

    Update path of user urls in urlpatterns of project urls.py

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
     path("api/airport/", include("airport.urls", namespace="airport")),
-    path("api/user/", include("user.urls", namespace="user"))
+    path("api/users/", include("user.urls", namespace="user"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
