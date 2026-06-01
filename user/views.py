90c3374e5361f34fbd62299d8acc72755f072fc3
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-01 21:25:24 +0300

    Override method get_queryset for filtering logic by email and is_staff to UserAdminViewSet

--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from django.db.models import QuerySet
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
@@ -22,6 +23,21 @@ class UserAdminViewSet(ModelViewSet):
     )
     permission_classes = (IsAdminUser, )
     pagination_class = UserSetPagination
+
+    def get_queryset(self) -> QuerySet[User]:
+        queryset = self.queryset
+
+        if self.action == "list":
+            email = self.request.query_params.get("email")
+            is_staff = self.request.query_params.get("is_staff")
+
+            if email:
+                queryset = queryset.filter(email__icontains=email)
+
+            if is_staff:
+                queryset = queryset.filter(is_staff=bool(is_staff))
+
+        return queryset
 
     def get_serializer_class(self) -> type[UserSerializer]:
         if self.action in ("list", "retrieve"):
