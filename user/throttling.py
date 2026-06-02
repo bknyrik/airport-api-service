6abf58fe31a783836225d07508c8babc5b2fda06
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-02 10:53:56 +0300

    Add throttle class UserRateThrottle

--- user/throttling.py
+++ user/throttling.py
@@ -1,11 +1,29 @@
 from typing import TYPE_CHECKING
 
-from rest_framework.throttling import SimpleRateThrottle
+from rest_framework.throttling import (
+    SimpleRateThrottle,
+    UserRateThrottle as DRFUserRateThrottle
+)
 from rest_framework.request import Request
 
 
 if TYPE_CHECKING:
     from rest_framework.views import APIView
+
+
+class UserRateThrottle(DRFUserRateThrottle):
+
+    def get_cache_key(self, request: Request, view: "APIView") -> str | None:
+        if (
+            request.user and request.user.is_authenticated
+            and not request.user.is_staff
+        ):
+            return self.cache_format % {
+                "scope": self.scope,
+                "ident": request.user.pk
+            }
+
+        return None
 
 
 class AdminRateThrottle(SimpleRateThrottle):
