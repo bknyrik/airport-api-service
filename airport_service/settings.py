e65d51f96f3ddb76e9bed3c76e98525f79f90ab3
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-02 06:04:01 +0300

    Add AdminRateThrottle to DEFAULT_THROTTLE_CLASSES of REST_FRAMEWORK

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -44,6 +44,7 @@ REST_FRAMEWORK = {
     "DEFAULT_THROTTLE_CLASSES": (
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
+        "user.throttling.AdminRateThrottle",
     ),
     "DEFAULT_THROTTLE_RATES": {
         "anon": "50/day",
