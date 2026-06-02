9ed996064eea68ec411f9bf3c2a3d7df26122270
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-02 20:48:08 +0300

    Add key DEFAULT_FILTER_BACKENDS to REST_FRAMEWORK in project settings.py

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -52,6 +52,9 @@ REST_FRAMEWORK = {
         "admin": "1000/day"
     },
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+    )
 }
 
 
