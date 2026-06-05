6fdd49889aa0ca19d47923823c1628671e1270ba
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-05 21:53:24 +0300

    Add constant GOT_AIRPLANE_TYPE_INFO in airport responses.py of openapi

--- openapi/airport/responses.py
+++ openapi/airport/responses.py
@@ -7,3 +7,8 @@ GOT_AIRPLANE_TYPES_INFO = OpenApiResponse(
     response=serializers.AirplaneTypeSerializer,
     description="Got all airplane types",
 )
+
+GOT_AIRPLANE_TYPE_INFO = OpenApiResponse(
+    response=serializers.AirplaneTypeSerializer,
+    description="Got airplane type info"
+)
