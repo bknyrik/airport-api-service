eae5918d55c7c2260edf0f5da29c0af75101d3a4
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-05 16:05:26 +0300

    Add constant FILTER_BY_IS_STAFF in openapi parameters.py

--- openapi/parameters.py
+++ openapi/parameters.py
@@ -5,3 +5,9 @@ FILTER_BY_EMAIL_PATTERN = OpenApiParameter(
     name="email",
     description="Filter by characters, that are in email.",
 )
+
+FILTER_BY_IS_STAFF = OpenApiParameter(
+    name="is_staff",
+    type=bool,
+    description="Filter by status if user is an admin."
+)
