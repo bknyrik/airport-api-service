d635eea2474bd19233cafc9b5329c6d0753eea07
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-05 11:09:59 +0300

    Add constant EMAIL_EXISTS in openapi examples.py

--- openapi/examples.py
+++ openapi/examples.py
@@ -5,3 +5,8 @@ EMAIL_IS_INVALID = OpenApiExample(
     name="Email is invalid",
     value={"email": ["Enter a valid email address."]}
 )
+
+EMAIL_EXISTS = OpenApiExample(
+    name="Email already exists",
+    value={"email": ["user with this Email address already exists."]}
+)
