de85537c5f5224a3b941756749bfb56dc891519d
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-05 21:31:41 +0300

    Remove USER_CANT_DELETE

--- openapi/user/responses.py
+++ openapi/user/responses.py
@@ -92,17 +92,6 @@ USER_NOT_FOUND = OpenApiResponse(
     response={"example": {"detail": "No User matches the given query."}}
 )
 
-USER_CANT_DELETE = OpenApiResponse(
-    description=(
-            "A user, that is not an admin, can't delete the user"
-    ),
-    response={
-        "example": {
-            "detail": ["You do not have permission to perform this action."]
-        }
-    }
-)
-
 USER_CANT_UPDATE = OpenApiResponse(
     description="A user, that is not an admin, can't update the user",
     response={
