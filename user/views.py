d79e2ff5facfbd353c4208e36cf6c2ad91880a9f
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-05 19:13:41 +0300

    Decorate method post of TokenVerifyView

--- user/views.py
+++ user/views.py
@@ -255,5 +255,15 @@ class TokenRefreshView(JWTTokenRefreshView):
 
 class TokenVerifyView(JWTTokenVerifyView):
 
+    @extend_schema(
+        summary="Verify the token",
+        responses={
+            status.HTTP_200_OK: responses.TOKEN_IS_VALID,
+            status.HTTP_401_UNAUTHORIZED: (
+                responses.TOKEN_IS_INVALID_OR_EXPIRED
+            ),
+            status.HTTP_429_TOO_MANY_REQUESTS: responses.REQUEST_IS_THROTTLED
+        }
+    )
     def post(self, request: Request, *args, **kwargs) -> Response:
         return super().post(request, *args, **kwargs)
