befe6c3ba4d6b676ff4cce4b665aad7cc3caedcb
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-05 17:35:57 +0300

    Replace TokenRefreshView in urlpatterns of project urls.py

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework_simplejwt.views import (
-    TokenRefreshView,
     TokenVerifyView
 )
 from drf_spectacular.views import (
@@ -28,7 +27,10 @@ from drf_spectacular.views import (
     SpectacularRedocView
 )
 
-from user.views import TokenObtainPairView
+from user.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 
 urlpatterns = [
