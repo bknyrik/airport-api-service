378e6fa63490c9c72c8ab8fc8c44ce770c9acfbe
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-06 09:50:55 +0300

    Add constant AIRPLANE_TYPE_NAME_IS_BLANK in airport examples.py of openapi

--- openapi/airport/examples.py
+++ openapi/airport/examples.py
@@ -5,3 +5,8 @@ AIRPLANE_TYPE_NAME_EXISTS = OpenApiExample(
     name="Airplane type exists",
     value={"name": ["airplane type with this name already exists."]}
 )
+
+AIRPLANE_TYPE_NAME_IS_BLANK = OpenApiExample(
+    name="Name is blank",
+    value={"name": ["This field may not be blank."]}
+)
