86141f961da0b917d35d85f52163cb4fedf2558b
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-06 15:29:45 +0300

    Update responses for patch of ManageUserAPIView

--- user/views.py
+++ user/views.py
@@ -198,12 +198,7 @@ class ManageUserAPIView(generics.RetrieveUpdateAPIView):
             "updated information about the current authorized user."
         ),
         summary="Partially update the current user",
-        responses={
-            status.HTTP_200_OK: responses.USER_UPDATED_SUCCESSFULLY,
-            status.HTTP_400_BAD_REQUEST: responses.INVALID_USER_DATA,
-            status.HTTP_401_UNAUTHORIZED: responses.USER_IS_NOT_AUTHORIZED,
-            status.HTTP_429_TOO_MANY_REQUESTS: responses.REQUEST_IS_THROTTLED
-        }
+        responses=responses.USER_PATCH_RESPONSES
     )
     def patch(self, request: Request, *args, **kwargs) -> Response:
         return super().patch(request, *args, **kwargs)
