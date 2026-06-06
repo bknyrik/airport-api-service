188ac2926c1c8dbdf3c6366c90c8026e8457982a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-06 09:54:53 +0300

    Update name of AIRPLANE_TYPE_NAME_EXISTS

--- openapi/airport/examples.py
+++ openapi/airport/examples.py
@@ -2,7 +2,7 @@ from drf_spectacular.openapi import OpenApiExample
 
 
 AIRPLANE_TYPE_NAME_EXISTS = OpenApiExample(
-    name="Airplane type exists",
+    name="Name already exists",
     value={"name": ["airplane type with this name already exists."]}
 )
 
