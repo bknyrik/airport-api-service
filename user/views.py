5f4294288cf73813788aca67bd44c4688f2bdd61
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-06 17:55:34 +0300

    Update responses for update of UserAdminViewSet

--- user/views.py
+++ user/views.py
@@ -86,14 +86,7 @@ class UserAdminViewSet(ModelViewSet):
             "about the user by id."
         ),
         summary="Completely update the user by id",
-        responses={
-            status.HTTP_200_OK: responses.USER_ADMIN_IS_CREATED,
-            status.HTTP_400_BAD_REQUEST: responses.USER_ADMIN_INVALID_DATA,
-            status.HTTP_401_UNAUTHORIZED: responses.USER_IS_NOT_AUTHORIZED,
-            status.HTTP_403_FORBIDDEN: responses.USER_IS_NOT_ADMIN,
-            status.HTTP_404_NOT_FOUND: responses.USER_NOT_FOUND,
-            status.HTTP_429_TOO_MANY_REQUESTS: responses.REQUEST_IS_THROTTLED
-        }
+        responses=responses.USER_ADMIN_UPDATE_RESPONSES
     )
     def update(self, request: Request, *args, **kwargs) -> Response:
         return super().update(request, *args, **kwargs)
