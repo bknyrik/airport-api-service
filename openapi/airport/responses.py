7267f72119b16ffc30ec79dafc7c8e2d02a293b2
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-07 21:00:54 +0300

    Add constant GOT_CREW_INFO in airport responses.py of openapi

--- openapi/airport/responses.py
+++ openapi/airport/responses.py
@@ -265,6 +265,11 @@ GOT_CREWMEMBERS_INFO = OpenApiResponse(
     response=serializers.CrewListRetrieveSerializer
 )
 
+GOT_CREW_INFO = OpenApiResponse(
+    description="Got information about crewmember",
+    response=serializers.CrewListRetrieveSerializer
+)
+
 CREW_LIST_RESPONSES = {
     status.HTTP_200_OK: GOT_CREWMEMBERS_INFO,
     status.HTTP_404_NOT_FOUND: user_responses.PAGE_NOT_FOUND,
