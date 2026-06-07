f87a04b6d55591d499389076eb30b6f31c989e16
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-07 21:05:52 +0300

    Add constant ROLE_INVALID_CHOICE in airport examples.py of openapi

--- openapi/airport/examples.py
+++ openapi/airport/examples.py
@@ -121,3 +121,8 @@ LAST_NAME_IS_BLANK = OpenApiExample(
     name="Last name is blank",
     value={"Last_name": ["This field may not be blank."]}
 )
+
+ROLE_INVALID_CHOICE = OpenApiExample(
+    name="Role has invalid choice",
+    value={"role": ["\"BB\" is not a valid choice."]}
+)
