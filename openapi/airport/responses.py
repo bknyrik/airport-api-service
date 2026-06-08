f4328275bf837c965bb699d00918a2d3d15a37d3
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-08 17:20:50 +0300

    Add constant ORDER_LIST_RESPONSES in airport responses.py of openapi

--- openapi/airport/responses.py
+++ openapi/airport/responses.py
@@ -496,3 +496,11 @@ GOT_ORDERS_INFO = OpenApiResponse(
     description="Got information about orders",
     response=serializers.OrderListRetrieveSerializer
 )
+
+ORDER_LIST_RESPONSES = {
+    status.HTTP_200_OK: GOT_ORDERS_INFO,
+    status.HTTP_401_UNAUTHORIZED: user_responses.USER_IS_UNAUTHORIZED,
+    status.HTTP_403_FORBIDDEN: user_responses.USER_IS_NOT_ADMIN,
+    status.HTTP_404_NOT_FOUND: user_responses.PAGE_NOT_FOUND,
+    status.HTTP_429_TOO_MANY_REQUESTS: user_responses.USER_REACHED_LIMIT_REQUESTS
+}
