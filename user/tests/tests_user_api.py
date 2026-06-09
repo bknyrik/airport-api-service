3c6141097cfb2397738944d87ad13e2251ff4914
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-09 19:31:29 +0300

    Add test test_user_admin_retrieve in AuhenticatedAdminUserApiTests

--- user/tests/tests_user_api.py
+++ user/tests/tests_user_api.py
@@ -166,3 +166,12 @@ class AuthenticatedAdminUserApiTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(serializer.data, response.data["results"])
+
+    def test_user_admin_retrieve(self) -> None:
+        user = user_sample()
+
+        response = self.client.get(user_admin_detail_url(pk=user.id))
+        serializer = UserAdminListRetrieveSerializer(user)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(serializer.data, response.data)
