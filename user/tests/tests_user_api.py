60bd97c12746b55ab6715ab619b886c9e21444f8
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-09 14:09:43 +0300

    Add constant USER_REGISTER_URL in tests_user_api.py

--- user/tests/tests_user_api.py
+++ user/tests/tests_user_api.py
@@ -8,6 +8,7 @@ from user.serializers import UserSerializer
 User = get_user_model()
 
 USER_ADMIN_LIST_URL = reverse("user:user-list")
+USER_REGISTER_URL = reverse("user:register")
 
 
 class UnauthenticatedUserApiTests(APITestCase):
