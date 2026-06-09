d1dde18623ae39679859907d0aef0549cddfc792
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-09 14:14:56 +0300

    Refactor test_user_admin_retrieve_login_required

--- user/tests/tests_user_api.py
+++ user/tests/tests_user_api.py
@@ -23,9 +23,7 @@ class UnauthenticatedUserApiTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_user_admin_retrieve_login_required(self) -> None:
-        url = reverse("user:user-detail", kwargs={"pk": 999})
-        response = self.client.get(url)
-
+        response = self.client.get(user_admin_detail_url(999))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_user_manage_login_required(self) -> None:
