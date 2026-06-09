4f9002bfbfcaac2eb9c972302ac6ad9997277235
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-09 13:36:31 +0300

    Add test test_user_admin_retrieve_login_required in UnauthenticatedUserApiTests

--- user/tests/tests_user_api.py
+++ user/tests/tests_user_api.py
@@ -11,3 +11,9 @@ class UnauthenticatedUserApiTests(APITestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_user_admin_retrieve_login_required(self) -> None:
+        url = reverse("user:user-detail", kwargs={"pk": 999})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
