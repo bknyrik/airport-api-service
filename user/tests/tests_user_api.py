40fd68299742f56a7f8c590229c6c353f5af6715
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-09 16:33:02 +0300

    Add test test_register_user_is_forbidden in AuthenticatedUserApiTests

--- user/tests/tests_user_api.py
+++ user/tests/tests_user_api.py
@@ -94,3 +94,7 @@ class AuthenticatedUserApiTests(APITestCase):
         serializer = UserSerializer(self.user)
 
         self.assertEqual(serializer.data, response.data)
+
+    def test_register_user_is_forbidden(self) -> None:
+        response = self.client.post(USER_REGISTER_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
