44388744601967cda714feb81f537e99eb7af98a
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-10 19:05:33 +0300

    Add test test_airport_destroy_authentication_required in UnauthenticatedAirportApiTests

--- airport/tests/tests_airport_api.py
+++ airport/tests/tests_airport_api.py
@@ -40,3 +40,7 @@ class UnauthenticatedAirportApiTests(APITestCase):
             data=data
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_airport_destroy_authentication_required(self) -> None:
+        response = self.client.delete(get_airport_detail_url(pk=1))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
