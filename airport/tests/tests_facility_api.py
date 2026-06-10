d9d1915d8d5347807af52564ba113b407b110085
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-10 18:32:14 +0300

    Add test test_facility_update in AuthenticatedAdminFacilityApiTests

--- airport/tests/tests_facility_api.py
+++ airport/tests/tests_facility_api.py
@@ -145,3 +145,20 @@ class AuthenticatedAdminFacilityApiTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(serializer.data, response.data)
+
+    def test_facility_update(self) -> None:
+        facility = facility_sample()
+        data = {
+            "name": "Another facility name",
+            "description": "Another facility description"
+        }
+        response = self.client.put(
+            get_facility_detail_url(pk=facility.id),
+            data=data
+        )
+        serializer = FacilitySerializer(facility)
+
+        facility.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(serializer.data, response.data)
