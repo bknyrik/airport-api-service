e32c78a9f1e39ff9f9df19039e6f0a84ae39dea2
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-10 10:06:20 +0300

    Refactor test_user_manage_request_is_throttled

--- user/tests/tests_user_api.py
+++ user/tests/tests_user_api.py
@@ -136,7 +136,10 @@ class AuthenticatedUserApiTests(APITestCase):
 
         response = self.client.get(USER_MANAGE_URL)
 
-        self.assertEqual(response.status_code, status.HTTP_429_TOO_MANY_REQUESTS)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_429_TOO_MANY_REQUESTS
+        )
 
 
 class AuthenticatedAdminUserApiTests(APITestCase):
