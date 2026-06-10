284418366500ef33c28efc6fe23fd99c98cfad74
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-10 18:24:04 +0300

    Add test case AuthenticatedAdminFacilityApiTests

--- airport/tests/tests_facility_api.py
+++ airport/tests/tests_facility_api.py
@@ -121,3 +121,14 @@ class AuthenticatedFacilityApiTests(APITestCase):
     def test_facility_destroy_is_forbidden(self) -> None:
         response = self.client.delete(get_facility_detail_url(pk=1))
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AuthenticatedAdminFacilityApiTests(APITestCase):
+
+    def setUp(self) -> None:
+        self.admin_user = User.objects.create_user(
+            email="admin@test.com",
+            password="adminpass12345",
+            is_staff=True
+        )
+        self.client.force_authenticate(user=self.admin_user)
