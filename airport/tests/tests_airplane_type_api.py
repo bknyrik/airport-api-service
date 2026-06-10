7b267fab944678bdd96412c392256989a9d89e07
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-10 10:37:58 +0300

    Add test case UnauthenticatedAirplaneTypeApiTests with tests test_airplane_type_list

--- airport/tests/tests_airplane_type_api.py
+++ airport/tests/tests_airplane_type_api.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.shortcuts import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from airport.models import AirplaneType
+from airport.serializers import AirplaneTypeSerializer
+
+
+AIRPLANE_TYPE_LIST_URL = reverse("airport:airplane-type-list")
+
+
+def airplane_type_sample(**kwargs) -> AirplaneType:
+    defaults = {
+        "name": "Airplane Sample",
+    }
+    defaults.update(kwargs)
+    return AirplaneType.objects.create(**defaults)
+
+
+class UnauthenticatedAirplaneTypeApiTests(APITestCase):
+
+    def test_airplane_type_list(self) -> None:
+        airplane_type = airplane_type_sample()
+        airplane_type2 = airplane_type_sample(name="Airplane Sample 2")
+
+        response = self.client.get(AIRPLANE_TYPE_LIST_URL)
+        serializer = AirplaneTypeSerializer(
+            (airplane_type, airplane_type2),
+            many=True
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(serializer.data, response.data["results"])
