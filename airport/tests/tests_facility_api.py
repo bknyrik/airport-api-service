3445deb26cdb4ec923d87e691d25413114ba2959
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-11 18:02:11 +0300

    Add test test_facility_partial_update_has_bad_request in AuthenticatedAdminFacilityApiTests

--- airport/tests/tests_facility_api.py
+++ airport/tests/tests_facility_api.py
@@ -189,6 +189,13 @@ class AuthenticatedAdminFacilityApiTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(serializer.data, response.data)
 
+    def test_facility_partial_update_has_bad_request(self) -> None:
+        another_facility = facility_sample(name="Another facility")
+        URL = get_facility_detail_url(pk=self.facility.id)
+        data = {"name": another_facility.name}
+        response = self.client.patch(URL, data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_facility_destroy(self) -> None:
         URL = get_facility_detail_url(pk=self.facility.id)
         response = self.client.delete(URL)
