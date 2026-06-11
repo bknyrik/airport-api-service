40221598cad3d5d9a4eebeda1ebc3c88b9d15677
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-11 11:37:47 +0300

    Add test test_crew_retrieve_authentication_required in UnauthenticatedCrewApiTests

--- airport/tests/tests_crew_api.py
+++ airport/tests/tests_crew_api.py
@@ -6,8 +6,17 @@ from rest_framework import status
 CREW_LIST_URL = reverse("airport:crew-list")
 
 
+def get_crew_detail_url(pk: int) -> str:
+    return reverse("airport:crew-detail", kwargs={"pk": pk})
+
+
+
 class UnauthenticatedCrewApiTests(APITestCase):
 
     def test_crew_list_authentication_required(self) -> None:
         response = self.client.get(CREW_LIST_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_crew_retrieve_authentication_required(self) -> None:
+        response = self.client.get(get_crew_detail_url(pk=1))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
