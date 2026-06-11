915c068c4335d485dba67fd8b7ed18e901b4ca10
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-11 17:26:07 +0300

    Add test test_crew_partial_update in AuthenticatedAdminCrewApiTests

--- airport/tests/tests_crew_api.py
+++ airport/tests/tests_crew_api.py
@@ -263,6 +263,20 @@ class AuthenticatedAdminCrewApiTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_crew_partial_update(self) -> None:
+        data = {"role": "CP"}
+        response = self.client.patch(
+            get_crew_detail_url(pk=self.crew.id),
+            data=data
+        )
+        serializer = CrewSerializer(self.crew)
+
+        self.crew.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(serializer.data, response.data)
+
+
     def test_crew_destroy(self) -> None:
         response = self.client.delete(get_crew_detail_url(pk=self.crew.id))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
