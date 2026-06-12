2621b28064ecf39f0639daaf598f1e7dc148d1c7
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-12 22:02:33 +0300

    Remove test_airplane_type_list_request_is_throttled from AuthenticatedAirplaneTypeApiTests

--- airport/tests/tests_airplane_type_api.py
+++ airport/tests/tests_airplane_type_api.py
@@ -153,18 +153,6 @@ class AuthenticatedAirplaneTypeApiTests(APITestCase):
         response = self.client.delete(URL)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_airplane_type_list_request_is_throttled(self) -> None:
-        USER_RATE = get_throttle_rate("user")
-
-        for _ in range(USER_RATE):
-            self.client.get(AIRPLANE_TYPE_LIST_URL)
-
-        response = self.client.get(AIRPLANE_TYPE_LIST_URL)
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_429_TOO_MANY_REQUESTS
-        )
-
     def test_airplane_type_retrieve_request_is_throttled(self) -> None:
         airplane_type = airplane_type_sample()
         USER_RATE = get_throttle_rate("user")
