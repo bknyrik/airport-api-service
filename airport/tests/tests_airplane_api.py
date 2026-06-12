3887b4d772c4f33740ef2fa985673a38d9bc31a1
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-12 20:22:21 +0300

    Add test test_airplane_list_filter_by_airplane_type in UnauthenticatedAirplaneApiTests

--- airport/tests/tests_airplane_api.py
+++ airport/tests/tests_airplane_api.py
@@ -47,3 +47,19 @@ class UnauthenticatedAirplaneApiTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(serializer.data, response.data["results"])
+
+    def test_airplane_list_filter_by_airplane_type(self) -> None:
+        response = self.client.get(
+            AIRPLANE_LIST_URL,
+            query_params={"airplane_type_id": self.airplane_type_2.id}
+        )
+        serializer_airplane_1 = AirplaneListRetrieveSerializer(
+            self.airplane_1
+        )
+        serializer_airplane_2 = AirplaneListRetrieveSerializer(
+            self.airplane_2
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotIn(serializer_airplane_1.data, response.data["results"])
+        self.assertIn(serializer_airplane_2.data, response.data["results"])
