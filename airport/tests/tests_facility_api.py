af3773950c3f430862496d37e8ee4ad2da071932
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-12 10:22:12 +0300

    Add test test_facility_retrieve_request_is_throttled in AuthenticatedFacilityApiTests

--- airport/tests/tests_facility_api.py
+++ airport/tests/tests_facility_api.py
@@ -136,6 +136,7 @@ class AuthenticatedFacilityApiTests(APITestCase):
             password="testpass12345"
         )
         self.client.force_authenticate(user=self.user)
+        self.facility = facility_sample()
 
     def test_facility_list_request_is_throttled(self) -> None:
         USER_RATE = get_throttle_rate("user")
@@ -144,6 +145,20 @@ class AuthenticatedFacilityApiTests(APITestCase):
             self.client.get(FACILITY_LIST_URL)
 
         response = self.client.get(FACILITY_LIST_URL)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_429_TOO_MANY_REQUESTS
+        )
+
+    def test_facility_retrieve_request_is_throttled(self) -> None:
+        USER_RATE = get_throttle_rate("user")
+        URL = get_facility_detail_url(pk=self.facility.id)
+
+        for _ in range(USER_RATE):
+            self.client.get(URL)
+
+        response = self.client.get(URL)
 
         self.assertEqual(
             response.status_code,
