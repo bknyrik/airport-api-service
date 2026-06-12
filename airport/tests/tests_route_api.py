0f74fb280485cb8e45215e3d3c06c8ddd6fe64e0
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-13 00:59:23 +0300

    Add test test_route_list in AuthenticatedRouteApiTests

--- airport/tests/tests_route_api.py
+++ airport/tests/tests_route_api.py
@@ -3,6 +3,11 @@ from django.shortcuts import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
+from airport.models import Route, Airport
+from airport.serializers import RouteListRetrieveSerializer
+from airport.tests.tests_airport_api import airport_sample
+
+
 User = get_user_model()
 
 
@@ -11,6 +16,14 @@ ROUTE_LIST_URL = reverse("airport:route-list")
 
 def get_route_detail_url(pk: int) -> str:
     return reverse("airport:route-detail", kwargs={"pk": pk})
+
+
+def route_sample(**kwargs) -> Route:
+    default = {
+        "distance": 250
+    }
+    default.update(kwargs)
+    return Route.objects.create(**default)
 
 
 class UnauthenticatedRouteApiTests(APITestCase):
@@ -62,4 +75,30 @@ class AuthenticatedRouteApiTests(APITestCase):
             email="user@airport.com",
             password="userpass12345"
         )
+        self.airport_1 = airport_sample()
+        self.airport_2 = airport_sample(
+            name="Airport Sample 2"
+        )
+        self.airport_3 = airport_sample(
+            name="Airport Sample 3"
+        )
+        self.route_1 = route_sample(
+            source_id=self.airport_1.id,
+            destination_id=self.airport_2.id,
+        )
+        self.route_2 = route_sample(
+            source_id=self.airport_2.id,
+            destination_id=self.airport_3.id,
+            distance=500
+        )
         self.client.force_authenticate(user=self.user)
+
+    def test_route_list(self) -> None:
+        response = self.client.get(ROUTE_LIST_URL)
+        serializer = RouteListRetrieveSerializer(
+            Route.objects.all(),
+            many=True
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(serializer.data, response.data["results"])
