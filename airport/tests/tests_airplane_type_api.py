b534c4bdd57b12f802601ce5c8c7b573c3706ff3
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-12 10:31:38 +0300

    Override method setUp to UnauthenticatedAirplaneTypeApiTests

--- airport/tests/tests_airplane_type_api.py
+++ airport/tests/tests_airplane_type_api.py
@@ -2,6 +2,7 @@ from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase
 from rest_framework import status
+from rest_framework.throttling import default_cache
 
 from airport.models import AirplaneType
 from airport.serializers import AirplaneTypeSerializer
@@ -26,6 +27,12 @@ def get_airplane_type_detail_url(pk: int) -> str:
 
 
 class UnauthenticatedAirplaneTypeApiTests(APITestCase):
+
+    def setUp(self) -> None:
+        default_cache.clear()
+        self.airplane_type_1 = airplane_type_sample()
+        self.airplane_type_2 = airplane_type_sample(name="Airplane Sample 2")
+        self.airplane_type_3 = airplane_type_sample(name="Airplane Sample 3")
 
     def test_airplane_type_list(self) -> None:
         airplane_type = airplane_type_sample()
