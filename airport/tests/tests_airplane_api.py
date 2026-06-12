a7b8d3910e8177edbe55dd6a0d011424fd8e4624
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-12 20:26:54 +0300

    Add test test_airplane_list_with_pagination in UnauthenticatedAirplaneApiTests

--- airport/tests/tests_airplane_api.py
+++ airport/tests/tests_airplane_api.py
@@ -79,3 +79,20 @@ class UnauthenticatedAirplaneApiTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn(serializer_airplane_1.data, response.data["results"])
         self.assertNotIn(serializer_airplane_2.data, response.data["results"])
+
+    def test_airplane_list_with_pagination(self) -> None:
+        PAGE_SIZE = 1
+        response = self.client.get(
+            AIRPLANE_LIST_URL,
+            query_params={"page_size": PAGE_SIZE}
+        )
+        serializer_airplane_1 = AirplaneListRetrieveSerializer(
+            self.airplane_1
+        )
+        serializer_airplane_2 = AirplaneListRetrieveSerializer(
+            self.airplane_2
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn(serializer_airplane_1.data, response.data["results"])
+        self.assertNotIn(serializer_airplane_2.data, response.data["results"])
