797295c0ba18a7bb72b4a66eb785851a01f66d41
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-12 14:45:55 +0300

    Refactor test_crew_destroy_authentication_required

--- airport/tests/tests_crew_api.py
+++ airport/tests/tests_crew_api.py
@@ -58,7 +58,8 @@ class UnauthenticatedCrewApiTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_crew_destroy_authentication_required(self) -> None:
-        response = self.client.delete(get_crew_detail_url(pk=1))
+        URL = get_crew_detail_url(pk=1)
+        response = self.client.delete(URL)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
