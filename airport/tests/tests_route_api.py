22d67b95694c9017a1c1a28f3c1befd77fcba827
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-13 00:44:50 +0300

    Add test test_route_retrieve_authentication_required in UnauthenticatedRouteApiTests

--- airport/tests/tests_route_api.py
+++ airport/tests/tests_route_api.py
@@ -6,8 +6,17 @@ from rest_framework import status
 ROUTE_LIST_URL = reverse("airport:route-list")
 
 
+def get_route_detail_url(pk: int) -> str:
+    return reverse("airport:route-detail", kwargs={"pk": pk})
+
+
 class UnauthenticatedRouteApiTests(APITestCase):
 
     def test_route_list_authentication_required(self) -> None:
         response = self.client.get(ROUTE_LIST_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_route_retrieve_authentication_required(self) -> None:
+        url = get_route_detail_url(pk=1)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
