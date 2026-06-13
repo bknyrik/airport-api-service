9ab174d4ffe23ed8e8e254c121eaf61358b3c060
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-13 18:38:43 +0300

    Add test test_order_retrieve_authentication_required in UnauthenticatedOrderApiTests

--- airport/tests/tests_order_api.py
+++ airport/tests/tests_order_api.py
@@ -6,8 +6,17 @@ from rest_framework import status
 ORDER_LIST_URL = reverse("airport:order-list")
 
 
+def get_order_detail_url(pk: int) -> str:
+    return reverse("airport:order-detail", kwargs={"pk": pk})
+
+
 class UnauthenticatedOrderApiTests(APITestCase):
 
     def test_order_list_authentication_required(self) -> None:
         response = self.client.get(ORDER_LIST_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_order_retrieve_authentication_required(self) -> None:
+        url = get_order_detail_url(pk=1)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
