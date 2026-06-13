28e1f8bfd7b13e5fd4c2c20988fe602b3ce1725e
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-13 14:05:03 +0300

    Add test test_flight_retrieve_authentication_required in UnauthenticatedFlightApiTests

--- airport/tests/tests_flight_api.py
+++ airport/tests/tests_flight_api.py
@@ -6,8 +6,17 @@ from rest_framework import status
 FLIGHT_LIST_URL = reverse("airport:flight-list")
 
 
+def get_flight_detail_url(pk: int) -> str:
+    return reverse("airport:flight-detail", kwargs={"pk": pk})
+
+
 class UnauthenticatedFlightApiTests(APITestCase):
 
     def test_flight_list_authentication_required(self) -> None:
         response = self.client.get(FLIGHT_LIST_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_flight_retrieve_authentication_required(self) -> None:
+        url = get_flight_detail_url(pk=1)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
