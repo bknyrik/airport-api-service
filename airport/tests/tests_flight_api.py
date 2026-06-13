5a26ee14684e805811456bb8f8735316599000d5
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-13 14:10:13 +0300

    Add test test_flight_create_authentication_required in UnauthenticatedFlightApiTests

--- airport/tests/tests_flight_api.py
+++ airport/tests/tests_flight_api.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
@@ -19,4 +21,15 @@ class UnauthenticatedFlightApiTests(APITestCase):
     def test_flight_retrieve_authentication_required(self) -> None:
         url = get_flight_detail_url(pk=1)
         response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_flight_create_authentication_required(self) -> None:
+        data = {
+            "route": 1,
+            "airplane": 1,
+            "departure_time": datetime(year=2026, month=1, day=1),
+            "arrival_time": datetime(year=2026, month=2, day=4),
+            "crewmembers": (1, 2, 3)
+        }
+        response = self.client.post(FLIGHT_LIST_URL, data=data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
