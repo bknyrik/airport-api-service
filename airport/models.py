a49f3b635eb73945dd4e8efff4e89ad230c71ff7
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-06-13 20:42:21 +0300

    Override method save for Ticket

--- airport/models.py
+++ airport/models.py
@@ -366,5 +366,21 @@ class Ticket(models.Model):
             ValidationError
         )
 
+    def save(
+        self,
+        *,
+        force_insert: bool | tuple[models.base.ModelBase, ...] = False,
+        force_update: bool = False,
+        using: str = None,
+        update_fields: Iterable[str] | None = None,
+    ) -> None:
+        self.full_clean()
+        return super().save(
+            force_insert=force_insert,
+            force_update=force_update,
+            using=using,
+            update_fields=update_fields
+        )
+
     def __str__(self) -> str:
         return f"Row: {self.row} Seat: {self.seat} Flight: {self.flight_id}"
